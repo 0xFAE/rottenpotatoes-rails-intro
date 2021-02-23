dde0198de57cfec6a7152bbf834055e73be092dd
0xFAE <fae5461@tamu.edu>
2021-02-23 02:58:38 +0000

    messages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,41 +10,35 @@ class MoviesController < ApplicationController
     
     @all_ratings = Movie.all_ratings
     
-    if params[:ratings].nil? && params[:commit] 
+    if params[:ratings].nil? and params[:commit] 
       session.delete(:ratings) 
       session.delete(:sort) 
     end
     
+    @all_ratings = Movie.all_ratings
+    @checked = params[:ratings] || session[:ratings] || Hash[@all_ratings.map {|rating| [rating, 1]}]
     
+    sort = params[:sort] || session[:sort]
     
-    if !params[:sort].nil?
-       sort = params[:sort]
-      session[:sort] = sort
-    else
-      sort = session[:sort] || []
-    end
-    
-    if !params[:ratings].nil?
-      @checked = params[:ratings]
-      session[:ratings] = @checked
-    else
-      @checked = session[:ratings] || Hash[@all_ratings.map {|rating| [rating, 1]}]
-    end
-    
-  
-    if !params[:sort] && !params[:ratings] && params[:home] && params[:ratings] != session[:ratings]
-      @checked= Hash[@all_ratings.map {|rating| [rating, 1]}]
-      redirect_to movies_path(:ratings => @checked, :sort => sort) and return
-    end
-    
-    
-    
-    if sort == 'title'
+    if sort == "title"
       @title_header = 'bg-warning'
       @release_date_header = ''
-    elsif sort == 'release_date'
+    end
+    
+    if sort == "release_date"
       @release_date_header = 'bg-warning'
       @title_header = ''
+    end
+    
+
+    if params[:sort] != session[:sort] || params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @checked
+      
+      if !params[:ratings] && !params[:sort] && params[:home]
+        session[:ratings] = Hash[@all_ratings.map {|rating| [rating, 1]}]
+      end
+      redirect_to sort: session[:sort], ratings: session[:ratings] and return
     end
     
        @movies = Movie.where(rating: @checked.keys).order(sort) 
